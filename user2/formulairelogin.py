444bd31e724c10d07d0ea16c32fa352c15f0f592
WorkSmart Technology <calvinawamusafiri@gmail.com>
2026-01-28 14:44:07 +0200

    Correction strucuture projet
    Je viens de corriger la structure globale du projet. Et sur certaines lignes j'ai ajoutE des commentaires pour eclaircicement.

--- user2/formulairelogin.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-
-class login(AuthenticationForm):
-        username = forms.CharField(label='nom utilisateur', max_length=100)
-        password = forms.CharField(label='mot de passe', widget=forms.PasswordInput)
